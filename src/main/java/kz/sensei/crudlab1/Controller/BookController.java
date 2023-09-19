f4bd50743877c598fe0d41717e8dad3c4a824d38
Bakhytzhan Zhanarov <zhanarovbakh@gmail.com>
2023-09-20 03:49:50 +0600

    Add Cross

--- src/main/java/kz/sensei/crudlab1/Controller/BookController.java
+++ src/main/java/kz/sensei/crudlab1/Controller/BookController.java
@@ -4,12 +4,14 @@ import kz.sensei.crudlab1.Model.Book;
 import kz.sensei.crudlab1.Repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
     private final BookRepository bookRepository;
 
